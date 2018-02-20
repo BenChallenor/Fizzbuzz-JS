c1c915d2a57d345968389b637476d81946e44cc2
Ben <ben.challenor@btopenworld.com>
2018-02-20 13:24:00 +0000

    Refactored

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -4,19 +4,6 @@ describe('Fizzbuzz', function() {
     fizzbuzz = new FizzBuzz();
   });
 
-  // describe('knows when a number is', function() {
-  //   it('divisable by 3', function() {
-  //       expect(fizzbuzz._isDivisibleByThree(3)).toBe(true);
-  //   });
-  // });
-  //
-  //
-  // describe('knows when a number is NOT', function() {
-  //   it('divisable by 3', function() {
-  //     expect(fizzbuzz._isDivisibleByThree(1)).toBe(false);
-  //   });
-  // });
-
   describe('when playing, says', function() {
     it('"Fizz" when a number is divisible by 3', function() {
     expect(fizzbuzz.play(3)).toEqual("Fizz");
